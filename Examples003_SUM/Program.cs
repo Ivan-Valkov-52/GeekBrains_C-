ba85393baf9ebb49d14486c5a4c3e71400784ee1
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-03 09:40:30 +0300

    Examples003_SUM

---
+++ Examples003_SUM/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите первое число: ");
+string firstString = Console.ReadLine();
+int numberA = Convert.ToInt32(firstString);
+Console.Write("Введите второе число: ");
+string secondString = Console.ReadLine();
+int numberB = Convert.ToInt32(secondString);
+int sum = numberA + numberB;
+Console.WriteLine("Сумма двух чисел равна: " + sum);
