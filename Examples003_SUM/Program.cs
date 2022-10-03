715276332bcce86c2392f9f0ed9d714e92b6d941
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-03 10:38:49 +0300

    Добавил функцию new Random()

--- Examples003_SUM/Program.cs
+++ Examples003_SUM/Program.cs
@@ -1,8 +1,16 @@
-﻿Console.Write("Введите первое число: ");
-string firstString = Console.ReadLine();
-int numberA = Convert.ToInt32(firstString);
-Console.Write("Введите второе число: ");
-string secondString = Console.ReadLine();
-int numberB = Convert.ToInt32(secondString);
+﻿int numberA = new Random().Next(1,10); // 1 2 3 4 ... 9
+Console.WriteLine("Первое число равно: " + numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine("Второе число равно: " + numberB);
 int sum = numberA + numberB;
 Console.WriteLine("Сумма двух чисел равна: " + sum);
+
+// Вариант с введением чисел через консоль!
+// Console.Write("Введите первое число: ");
+// string firstString = Console.ReadLine();
+// int numberA = Convert.ToInt32(firstString);
+// Console.Write("Введите второе число: ");
+// string secondString = Console.ReadLine();
+// int numberB = Convert.ToInt32(secondString);
+// int sum = numberA + numberB;
+// Console.WriteLine("Сумма двух чисел равна: " + sum);
