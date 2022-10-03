0ebbb829d84721adca00e089a038d8f0399ffa2f
Ivan-Valkov-52 <ivanvalkov81299@gmail.com>
2022-10-03 12:12:02 +0300

    Examples006_Base

---
+++ Examples006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 8;
+int c = 3;
+int d = 12;
+int e = 6;
+int valueMax = a;
+
+if (valueMax < b) valueMax = b;
+if (valueMax < c) valueMax = c;
+if (valueMax < d) valueMax = d;
+if (valueMax < e) valueMax = e;
+
+Console.WriteLine("Максимальное число равно: " + valueMax);
